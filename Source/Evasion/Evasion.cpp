2a237276d6b73eca1a5ccdc92123801e9e039f65
YacineL <yacine9258@gmail.com>
2020-01-27 22:57:38 +0100

    Initial commit

---
+++ Source/Evasion/Evasion.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Evasion.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Evasion, "Evasion" );
